84949b102e5a5d5b0b8ddda437e008664f3b3db8
Jeremy <jeremyjoseph1@gmail.com>
2023-02-15 22:32:08 -0800

    Assignment 01

---
+++ js/script.js
@@ -0,0 +1,177 @@
+// STEP 1
+
+/*
+let someMonth
+function theMonth() {}
+let currentMonth
+let summerMonth
+let myLibraryFunction
+*/
+
+// STEP 2
+
+/*
+//numeric literal expression:
+120
+
+// a string literal expression:
+"backend"
+
+// a Boolean literal expression:
+true
+
+// null literal expression:
+null
+*/
+
+// STEP 3
+/*
+// complex / variable expressions:
+// example 1:
+let complexExpression1 = (1 + 2)*50/100;
+// example 2:
+let complexExpression2 = Math.random()*100;
+*/
+
+// STEP 4
+/*
+let strFirstName
+let strLastName
+let strAddress
+let strCity
+let strState
+let intZipCode
+let intYourAge
+let strReferralSource
+let boolMayWeContactYou
+*/
+
+// STEP 5
+/*
+intZipCode = 92109;
+strState = "California";
+boolMayWeContactYou = false;
+*/
+
+// STEP 6
+/*
+let step6 = "step" + 6;
+console.log(step6);
+*/
+
+// STEP 7
+/*
+let step7 = true + "step";
+let step7b = false + 7;
+console.log(step7);
+console.log(step7b);
+*/
+
+// STEP 8
+/*
+// the string was not valid. below is the fixed version
+let someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."'
+console.log(someString)
+*/
+
+// STEP 9
+/*
+let step9 = null;
+console.log(step9);
+let step9b;
+console.log(step9b);
+*/
+
+// STEP 10
+/*
+let step10;
+console.log(typeof("a"));
+console.log(typeof(1));
+console.log(typeof(true));
+console.log(typeof(Math));
+console.log(typeof(step10))
+*/
+
+// STEP 11
+/*
+alert("Hello " + "Jeremy Joseph, welcome to the JavaScript class!");
+*/
+
+// STEP 12
+/*
+let names = 'Jeremy';
+let step12 = "Hello " + names + " welcome to the JavaScript class!"
+alert(step12);
+*/
+
+// STEP 13
+/*
+let names = 'Jeremy';
+let course = "JavaScript";
+let step13 = "Hello " + names + " welcome to the " + course + " class!"
+alert(step13);
+*/
+
+// STEP 14
+/*
+let names = 'Jeremy';
+let course = "JavaScript";
+let step14 = "Hello " + names + "." + "\n" + "Welcome to the " + course + " class!"
+alert(step14);
+*/
+
+// STEP 15
+/*
+let names = prompt("Please enter your name");
+let course = "JavaScript";
+let step15 = "Hello " + names + "." + "\n" + "Welcome to the " + course + " class!"
+alert(step15);
+*/
+
+// STEP 16
+/*
+let names = prompt("Please enter your name");
+let course = prompt("Please enter your course");
+let step16 = "Hello " + names + "." + "\n" + "Welcome to the " + course + " class!"
+alert(step16);
+*/
+
+// STEP 17
+/*
+let x = 10;
+let y = 20;
+console.log(x+y);
+*/
+
+// STEP 18
+/*
+let x = 20;
+x+=20;
+console.log(x);
+*/
+
+// STEP 19
+/*
+let x = 20;
+x*=5;
+console.log(x);
+*/
+
+// STEP 20
+/*
+let x = 20%3;
+x/=1;
+console.log(x);
+*/
+
+// STEP 21
+/*
+let step = 21;
+console.log(step > 20 && step != 22)
+*/
+
+// STEP 22
+/*
+let step = 22
+console.log(step == 21 || step < 22 );
+*/
