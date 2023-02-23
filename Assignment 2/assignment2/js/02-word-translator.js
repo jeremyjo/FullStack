c54ebfde0a868842a23bf6529074ab223343ad56
Jeremy <jeremyjoseph1@gmail.com>
2023-02-23 00:19:37 -0800

    Completed Assignment 2 Task 2: World Translator

--- Assignment 2/assignment2/js/02-word-translator.js
+++ Assignment 2/assignment2/js/02-word-translator.js
@@ -1,2 +1,15 @@
 // The Word Translator
-
+let userLanguage = (prompt("Enter a language code: es, de, en, or fr"));
+switch(userLanguage.toLowerCase()) {
+    case("de"):
+        document.write("Hello World translated in German is: Hallo Welt");
+        break;
+    case("es"):
+        document.write("Hello World translated in Spanish is: Hola Mundo");
+        break;
+    case("fr"):
+        document.write("Hello World translated in French is: Bonjour le Monde");
+        break;
+    default:
+        document.write("Hello World");
+}
