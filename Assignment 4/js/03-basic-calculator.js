dd639c6d78888b06ffccb51b9013cf95cf2cd7ca
Jeremy <jeremyjoseph1@gmail.com>
2023-03-15 18:06:19 -0700

    added calculator functionality

--- Assignment 4/js/03-basic-calculator.js
+++ Assignment 4/js/03-basic-calculator.js
@@ -1,8 +1,38 @@
 // IMPORT THE MODULE
+import * as calc from './modules/calculator.js'
+// import 
 
 // COLLECT FIRST NUMBER FROM USER
+
+let userFirstNumber = parseInt(prompt("Enter the first number"));
+
 // COLLECT SECOND NUMBER FROM USER
+
+let userSecondNumber = parseInt(prompt("Enter the second number"));
+
 // COLLECT OPERATION TO PERFORM (+,-,*,/) FROM USER
 
+let userOperationInput = prompt("Enter the operation");
+
 // CHECK TO SEE WHAT OPERATION THEY'RE USING
+
+let userOperationString = cacalculate(userOperationInput);
+
 // CALL THE APPROPRIATE FUNCTION
+
+userOperationString(userFirstNumber, userSecondNumber);
+
+// switch (userOperationInput) {
+//     case "+":
+//         calc.add(userFirstNumber, userSecondNumber);
+//         break;
+//     case "-":
+//         calc.subtract(userFirstNumber, userSecondNumber);
+//         break;
+//     case "*":
+//         calc.multiply(userFirstNumber, userSecondNumber);
+//         break;
+//     case "/":
+//         calc.divide(userFirstNumber, userSecondNumber);
+//         break;
+// }
--- Assignment 4/js/modules/calculator.js
+++ Assignment 4/js/modules/calculator.js
@@ -1,13 +1,46 @@
 // ADD A PRIVATE FUNCTION CALLED CALCULATE
 
+function calculate (parameterString) {
+    switch (parameterString) {
+        case "+":
+            return "add"
+            break;
+        case "-":
+            return "subtract"
+            break;
+        case "*":
+            return "multiply"
+            break;
+        case "/":
+            return "divide"
+            break;
+    }
+}
+
 // ADD FOUR PUBLIC FUNCTIONS BELOW
 //////////////////////////////////
 // ADD FUNCTION
-
+function add(parameterNumber1, parameterNumber2) {
+    console.log("add");
+    alert(parameterNumber1 + parameterNumber2);
+}
 // SUBTRACT FUNCTION
+
+function subtract (parameterNumber1, parameterNumber2) {
+    alert(parameterNumber1 - parameterNumber2);
+}
 
 // MULTIPLY FUNCTION
 
+function multiply (parameterNumber1, parameterNumber2) {
+    alert(parameterNumber1 * parameterNumber2);
+}
+
 // DIVIDE FUNCTION
 
+function divide (parameterNumber1, parameterNumber2) {
+    alert(parameterNumber1 / parameterNumber2);
+}
+
 // EXPORT THE FOUR PUBLIC FUNCTIONS
+export {add, subtract, multiply, divide, calculate}
